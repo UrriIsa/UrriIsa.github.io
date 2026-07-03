803ecd0ae0fa2292626e5f62e5ea56ca600a3049
UrriIsa <isaaurrru@gmail.com>
2026-07-02 22:14:30 -0600

    fix[layout.tsx] :arregla referencia a los estilos.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"; // Cambiar extensión
+import "./style.css"; // Cambiar extensión
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
