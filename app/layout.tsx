1f0e007569303bca76a1913ce9de237c8cf8256a
UrriIsa <isaaurrru@gmail.com>
2026-08-12 20:33:09 -0600

    feat[layout.tsx] : añade fuentes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
 import type { Metadata } from "next";
 /*import { Geist, Geist_Mono } from "next/font/google";*/
+import {Space_Mono, Unbounded, DM_Serif_Display} from "next/font/google" ;
 import "./style.css";
 import Navbar from '@/components/Navbar';
 import Cursor from '@/components/Cursor';
 
-/*
+const spaceMono = Space_Mono ({
+  subsets : ["latin"],
+  weight : ["400", "700"],
+  style: ["normal", "italic"],
+  variable : "--fontMono"
+}) ;
 
-[FEAT] : añadir las fuentes así
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const unbounded = Unbounded ({
+  subsets : ["latin"],
+  weight : ["300", "400", "700", "900"],
+  variable : "--fontMono"
+}) ;
+const dmSerif = DM_Serif_Display({
   subsets: ["latin"],
+  weight: ["400"],
+  style: ["normal", "italic"],
+  variable: "--fontSerif",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});*/
 
 export const metadata: Metadata = {
   title: "Isaac Urrutia",
@@ -32,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${spaceMono.variable} ${dmSerif.variable} ${unbounded.variable}`}>
       <body>
           <Navbar/>
           <Cursor/>
